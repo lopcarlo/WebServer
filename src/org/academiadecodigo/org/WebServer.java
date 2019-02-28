c940c105fdd7d6487735d04e4433960436cc3fc4
Luis <lopcarlo@gmail.com>
2019-02-28 18:26:52 +0000

    first

---
+++ src/org/academiadecodigo/org/WebServer.java
@@ -0,0 +1,27 @@
+package org.academiadecodigo.org;
+
+/**
+ * Created by codecadet on 28/02/2019.
+ */
+public class WebServer {
+
+    public WebServer(){
+        start();
+    }
+
+
+    public void start(){
+
+
+
+
+
+    }
+
+    public static void main(String[] args) {
+        new WebServer();
+
+    }
+
+
+}
