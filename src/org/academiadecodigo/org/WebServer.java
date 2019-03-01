7b2f7a52d5a78f05f0b796c3b45ff53bd6f37ab9
Luis <lopcarlo@gmail.com>
2019-03-01 17:45:36 +0000

    Miss 404

--- src/org/academiadecodigo/org/WebServer.java
+++ src/org/academiadecodigo/org/WebServer.java
@@ -20,6 +20,7 @@ public class WebServer {
     private String extension;
     private String path = "/";
     private String type;
+    private double size;
 
 
     public WebServer(int portNumber) throws IOException {
@@ -49,12 +50,11 @@ public class WebServer {
 
         dataOut = new DataOutputStream(client.getOutputStream());
 
-        dataOut.writeBytes(header());
 
-        dataOut.write(data());
-
+        dataWrite();
 
     }
+
 
     public String header() {
 
@@ -66,22 +66,54 @@ public class WebServer {
 
     }
 
+    public String notFound() {
+
+        String str = "HTTP/1.0 404 Not Found\r\n" +
+                "Content-Type: text/html; charset=UTF-8\r\n" +
+                "Content-Length: " + size + " \r\n" +
+                "\r\n";
+
+        return str;
+    }
 
 
-    public byte[] data() throws IOException {
-        if(pathRequest().equals("/")){
+    public void dataWrite() throws IOException {
+        if (pathRequest().equals("/")) {
             path = "/Index.html";
         }
 
         file = new File("www" + path);
-        this.byteSize = file.length();
-        FileInputStream fileInput = new FileInputStream(file);
 
-        byte[] fileContent = new byte[10240000];
-        while (fileInput.read(fileContent) != -1) {
-            return fileContent;
+        if (file.canRead()) {
+
+            this.byteSize = file.length();
+            dataOut.writeBytes(header());
+
+            FileInputStream fileInput = new FileInputStream(file);
+
+            byte[] fileContent = new byte[1024];
+            while (fileInput.read(fileContent) != -1) {
+                dataOut.write(fileContent);
+            }
+
+        } else {
+
+            file = new File("www/404Error.html");
+            size = file.length();
+            System.out.println("is there a path:" + file.canRead());
+
+            dataOut.writeBytes(notFound());
+
+            System.out.println(notFound());
+
+            FileInputStream fileInput = new FileInputStream(file);
+            byte[] fileContent = new byte[1024];
+            while (fileInput.read(fileContent)!= -1){
+                dataOut.write(fileContent);
+            }
+           
         }
-        return null;
+
 
     }
 
@@ -91,25 +123,25 @@ public class WebServer {
 
     }
 
-    public String pathRequest(){
-       String[] arr;
-       arr = getRequest.split(" ");
+    public String pathRequest() {
+        String[] arr;
+        arr = getRequest.split(" ");
         System.out.println(arr[1]);
         path = arr[1];
         return arr[1];
 
     }
 
-    public String extension(){
+    public String extension() {
         extension = path.substring(path.lastIndexOf("."));
-        System.out.println("extensao "+ extension);
+        System.out.println("extensao " + extension);
         fileType();
         return extension;
 
     }
 
-    public void fileType(){
-        switch(extension){
+    public void fileType() {
+        switch (extension) {
             case ".html":
                 type = "text/html";
                 break;
@@ -119,11 +151,25 @@ public class WebServer {
             case ".jpeg":
                 type = "image/jpeg";
                 break;
+            case ".css":
+                type = "image/css";
+                break;
+            case ".gif":
+                type = "image/gif";
+                break;
+            case ".png":
+                type = "image/png";
+                break;
+            case ".wave":
+                type = "audio/wave";
+                break;
+            case ".ogg":
+                type = "video/jpeg";
+                break;
+
         }
 
     }
-
-
 
 
 }
